4648318e635a5b9cd67c3b220b33658915f41665
AmrHesham <eng.amrhesham.22@gmail.com>
2022-11-02 19:34:36 +0200

    dd

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 
 const corsOptions = {
-  origin: "*",
+  origin: "https://shopping-one-psi.vercel.app",
   credentials: true,
   optionSuccessStatus: 200,
 };
