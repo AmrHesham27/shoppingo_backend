140ffe8e2ec96cccd24e69ea70f1c8394a8d1711
AmrHesham <eng.amrhesham.22@gmail.com>
2022-11-02 23:54:20 +0200

    k

--- server.js
+++ server.js
@@ -3,13 +3,6 @@ const { dbConnect } = require("./models/dbconnection/dbconnection");
 
 dbConnect();
 
-if (process.env.ENV === "production" || process.env.ENV === "staging") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  });
-}
-
 app.listen(process.env.PORT, () => {
   console.log(`your project is running`);
 });
